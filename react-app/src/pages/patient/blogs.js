9fd0c847d785d54ae6d8e25439900422785fc9b4
nesriine200 <102911703+nesriine200@users.noreply.github.com>
2022-04-27 00:50:18 +0200

    Search filter blog

--- react-app/src/pages/patient/blogs.js
+++ react-app/src/pages/patient/blogs.js
@@ -6,6 +6,8 @@ import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
 function Blogs() {
   const [blogs, setBlog] = useState([]);
+  const [categories, setCategories] = useState([]);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,32 +17,260 @@ function Blogs() {
       setBlog(res.data);
     };
     getBlogs();
+    const getCategories = async () => {
+      const res = await axios("http://localhost:8080/blogs/categories");
+      console.log(res.data);
+      setCategories(res.data);
+    };
+    getCategories();
+    console.log(categories);
   }, []);
+
+
   const deletehandler = (id) => {
     axios
       .delete(`http://localhost:8080/blogs/delete-blog/${id}`)
       .then((res) => console.log(res.data));
       window.location.reload();
     };
+
+    const [search, setSearch] = useState("");
+    const [searchR, setSearchR] = useState([]);
+    const [filtreR, setFiltreR] = useState([]);
+
+    const [showResults, setShowResults] = React.useState(true)
+
+   
+    const onChangeSearch = (e) => {
+        const search = e.target.value;
+        setSearch(search);
+      };
+   
+      const handleSearch = (e) => {
+        e.preventDefault();
+   
+        const getSearchR = async () => {
+            const res = await axios("http://localhost:8080/blogs/search-blog/"+`${search}`);
+            setSearchR(res.data);
+          };
+        getSearchR();
+        setShowResults(false)
+    };
+
+    const handleFilter = async (filtre) => {
+      const res = await axios("http://localhost:8080/blogs/filter-blog/"+`${filtre}`)
+      .then((res) => setFiltreR(res.data));
+      console.log(filtreR);
+      setShowResults(false)
+
+      };
+
+
   return (
     <div className="page-wrapper">
       <div className="content">
-        <div class="row">
-          <div class="col-sm-8 col-4">
-            <h4 class="page-title">Blog</h4>
+      <div class="">
+            <h4 class="page-title">Blog</h4>    
           </div>
-          <div class="col-sm-4 col-8 text-right m-b-30">
-            <Link
+        <div class="row">
+         
+          <br></br>
+          <div class="col-sm-4 col-12 text-right m-b-30">
+            
+          <form className="search-form" onSubmit={handleSearch}>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Search..."
+                      className="form-control"
+                      value={search}
+                      onChange={onChangeSearch}
+                    />
+                    <div className="input-group-append">
+                      <button type="submit" className="btn btn-primary">
+                        <i className="fa fa-search" />
+                      </button>
+                    </div>
+                  </div>
+                </form>
+                <br/>
+                
+        
+          </div>
+      
+        </div>
+        
+        { showResults ?   <div className="row">
+        <aside >
+        <Link
               class="btn btn-primary btn-rounded float-right"
               to={"/patient/add-blog"}
               component={Link}
             >
               <i class="fa fa-plus"></i> Add Blog
             </Link>
-          </div>
-        </div>
-        <div className="row">
+            <div className="widget tags-widget">
+                <h5>Tags</h5>
+                <ul className="tags">
+                {categories.map((el, index) => {
+                  return(
+                  <li>
+                    <a onClick={() => { handleFilter(el); }} className="tag">
+                      {el}
+                    </a>
+                  </li> 
+                  );
+                })}
+    <li>
+                    <a href="#." className="tag">
+                      Family
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Medical
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Injection
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Secure
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Insurance
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Insurance
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Insurance
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Insurance
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#." className="tag">
+                      Insurance
+                    </a>
+                  </li>
+                  
+                </ul>
+              </div>
+
+              </aside>
           {blogs.map((el, index) => {
+            return (
+              
+              <div className="col-sm-6 col-md-6 col-lg-4">
+                <div className="dropdown profile-action ">
+                  <a
+                    href="#"
+                    className="action-icon dropdown-toggle"
+                    data-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <i className="fa fa-ellipsis-v" />
+                  </a>
+                  <div className="dropdown-menu dropdown-menu-right">
+                    <Link
+                      className="dropdown-item"
+                      to={`/patient/edit-blog/${el._id}`}
+                      component={Link}
+                    >
+                      <i className="fa fa-pencil m-r-5" /> Edit
+                    </Link>
+                    <a
+                      className="dropdown-item"
+                      href="#"
+                      data-toggle="modal"
+                      data-target="#delete_doctor"
+                      onClick={() => {
+                        deletehandler(el._id);
+                      }}
+                    >
+                      <i className="fa fa-trash-o m-r-5" /> Delete
+                    </a>
+                  </div>
+                </div>
+                <div className="blog grid-blog">
+                  <div className="blog-image">
+                    <Link
+                      className="dropdown-item"
+                      to={`/patient/blog-details/${el._id}`}
+                      component={Link}
+                    >
+                      <img
+                        className="img-fluid"
+                        src="assets/img/blog/blog-01.jpg"
+                        alt
+                      />
+                    </Link>
+                  </div>
+                  <div className="blog-content">
+                    <h3 className="blog-title">
+                      <Link to="/patient/blog-details" component={Link}>
+                        {el.title}
+                      </Link>
+                    </h3>
+                    <p>{el.description}</p>
+                    <Link
+                      className="read-more"
+                      to="/patient/blog-details"
+                      component={Link}
+                    >
+                      <i className="fa fa-long-arrow-right" /> Read More
+                    </Link>
+
+                    <div className="blog-info clearfix">
+                      <div className="post-left">
+                        <ul>
+                          <li>
+                            <a href="#.">
+                              <i className="fa fa-calendar" />{" "}
+                              {dayjs(`${el.dateCreation}`).format("DD/MM/YYYY")}{" "}
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="post-right">
+                        <a href="#.">
+                          <i className="fa fa-heart-o" />
+                          21
+                        </a>{" "}
+                        <a href="#.">
+                          <i className="fa fa-eye" />8
+                        </a>{" "}
+                        <a href="#.">
+                          <i className="fa fa-comment-o" />
+                          17
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          ;
+        </div> : null }
+      
+
+
+        <div className="row">
+          {searchR.map((el, index) => {
             console.log(el);
             return (
               <div className="col-sm-6 col-md-6 col-lg-4">
@@ -134,8 +364,105 @@ function Blogs() {
             );
           })}
           ;
-        </div>
-      </div>
+       </div>
+
+       <div className="row">
+          {filtreR.map((el, index) => {
+            console.log(el);
+            return (
+              <div className="col-sm-6 col-md-6 col-lg-4">
+                <div className="dropdown profile-action ">
+                  <a
+                    href="#"
+                    className="action-icon dropdown-toggle"
+                    data-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <i className="fa fa-ellipsis-v" />
+                  </a>
+                  <div className="dropdown-menu dropdown-menu-right">
+                    <Link
+                      className="dropdown-item"
+                      to={`/patient/edit-blog/${el._id}`}
+                      component={Link}
+                    >
+                      <i className="fa fa-pencil m-r-5" /> Edit
+                    </Link>
+                    <a
+                      className="dropdown-item"
+                      href="#"
+                      data-toggle="modal"
+                      data-target="#delete_doctor"
+                      onClick={() => {
+                        deletehandler(el._id);
+                      }}
+                    >
+                      <i className="fa fa-trash-o m-r-5" /> Delete
+                    </a>
+                  </div>
+                </div>
+                <div className="blog grid-blog">
+                  <div className="blog-image">
+                    <Link
+                      className="dropdown-item"
+                      to={`/patient/blog-details/${el._id}`}
+                      component={Link}
+                    >
+                      <img
+                        className="img-fluid"
+                        src="assets/img/blog/blog-01.jpg"
+                        alt
+                      />
+                    </Link>
+                  </div>
+                  <div className="blog-content">
+                    <h3 className="blog-title">
+                      <Link to="/patient/blog-details" component={Link}>
+                        {el.title}
+                      </Link>
+                    </h3>
+                    <p>{el.description}</p>
+                    <Link
+                      className="read-more"
+                      to="/patient/blog-details"
+                      component={Link}
+                    >
+                      <i className="fa fa-long-arrow-right" /> Read More
+                    </Link>
+
+                    <div className="blog-info clearfix">
+                      <div className="post-left">
+                        <ul>
+                          <li>
+                            <a href="#.">
+                              <i className="fa fa-calendar" />{" "}
+                              {dayjs(`${el.dateCreation}`).format("DD/MM/YYYY")}{" "}
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="post-right">
+                        <a href="#.">
+                          <i className="fa fa-heart-o" />
+                          21
+                        </a>{" "}
+                        <a href="#.">
+                          <i className="fa fa-eye" />8
+                        </a>{" "}
+                        <a href="#.">
+                          <i className="fa fa-comment-o" />
+                          17
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          ;
+       </div>
+   </div>
     </div>
   );
 }

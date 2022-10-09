a1274d9f5cadf4af20ab8fcd8054a96b930e2b63
Ahlem Laajili <78981558+ahlem-phantom@users.noreply.github.com>
2022-10-09 05:18:59 +0200

    updates

--- server/app/config/stripe.js
+++ server/app/config/stripe.js
@@ -1,6 +1,6 @@
 const configureStripe = require('stripe')
 
-const STRIPE_SECRET_KEY = 'pk_test_51KkgQ1BHNzRaB25PEYP77hzfRdhQQhRyu58DiteDA7sHdE4QG5gsOQYzUG9JPvGtF0eBFEvH7ZyMEXqnn3Nc71xQ00u2WOqoGf'
+const STRIPE_SECRET_KEY = ''
 
 const stripe = configureStripe(STRIPE_SECRET_KEY)
 
